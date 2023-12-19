5c0bc5b3a6ea0f6beed6266cfadafe4f0652aeba
Vishwas Nigam <90500935+VishwasNigam02@users.noreply.github.com>
2023-12-19 14:00:52 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,22 @@
 */
 
 function isAnagram(str1, str2) {
+  // Remove non-word characters and convert to lowercase
+  const cleanStr1 = str1.replace(/[^\w]/g, '').toLowerCase();
+  const cleanStr2 = str2.replace(/[^\w]/g, '').toLowerCase();
 
+  // Check if the lengths of the strings are equal
+  if (cleanStr1.length !== cleanStr2.length) {
+    return false;
+  }
+
+  // Sort the characters in both strings and compare
+  const sortedStr1 = cleanStr1.split('').sort().join('');
+  const sortedStr2 = cleanStr2.split('').sort().join('');
+
+  // Check if the sorted strings are equal
+  return sortedStr1 === sortedStr2;
 }
+
 
 module.exports = isAnagram;
